f8a451fc00a5a12991f4bb9e24152619c8dfdba7
AnggitaTyasmara <anggitotsmr@gmail.com>
2021-06-26 05:24:41 +0700

    Act 9

--- app/src/main/java/com/example/activity9/MainActivity.java
+++ app/src/main/java/com/example/activity9/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.activity9;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private Button bTambah, bLihat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bTambah = findViewById(R.id.btnTambah);
+        bLihat = findViewById(R.id.btnLihat);
+
+        bTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(TambahData.getActIntent(MainActivity.this));
+            }
+        });
+
+        bLihat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
